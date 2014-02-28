4145c9f255817ef2597a898a4dfdcd221991a60b
Brian <bri.hoang@gmail.com>
2014-02-27 19:19:08 -0600

    added verification system. global variables not working correctly yet

--- creep.py
+++ creep.py
@@ -5,23 +5,53 @@ from auth import authorize
 
 teamAName = "Dawson"
 teamBName = "Friendswood"
+teamAScore = 0
+teamBScore = 0
+verified_A_score = 0
+verified_B_score = 0
+possible_scores = {}
 
-def printScore(game_info):
+
+
+def VERIFICATION_RESET():
+	return 3
+
+def verify():
+	global possible_scores
+	global teamAScore
+	global teamBScore
+	for k in possible_scores.iterkeys():
+		if possible_scores[k] >= VERIFICATION_RESET():
+			teamAScore = k[0] 
+			teamBScore = k[1]
+			possible_scores.clear()
+			printScore()
+			break
+
+def processScore(game_info):
+	global possible_scores
 	words = game_info.split()
 	for x in range(len(words)):
 		if words[x].lower() == teamAName.lower():
-			teamAScore = words[x+1]
+			teamA = words[x+1]
 		elif words[x].lower() == teamBName.lower():
-			teamBScore = words[x+1]
-	print teamAName + " " + teamAScore
-	print teamBName + " " + teamBScore
+			teamB = words[x+1]
+	tup = (teamA, teamB)
+	if tup not in possible_scores.keys() and (teamA != teamAScore or teamBScore != teamBScore):
+		possible_scores[tup] = 0
+	if tup in possible_scores: 
+		possible_scores[tup] += 1
+	verify()
+
+def printScore():
+	print teamAName + " " + str(verified_A_score)
+	print teamBName + " " + str(verified_B_score)
 
 class StdOutListener(StreamListener):
 	def on_status(self, status):
 		tweet = status.text
-		
 		if teamAName.lower() in tweet.lower() and teamBName.lower() in tweet.lower():
-			printScore(tweet)
+			processScore(tweet)
 		return True
 
 	def on_error(self, status):
@@ -29,5 +59,6 @@ class StdOutListener(StreamListener):
 
 l = StdOutListener()
 auth = authorize() 
+
 stream = tweepy.Stream(auth, l)
 stream.filter(track=['dawson'])
